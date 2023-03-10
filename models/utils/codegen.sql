bb55d5bb37764b99aebfba594aaaf0f4a00103b9
Anders Swanson <anders.swanson@dbtlabs.com>
2023-03-10 15:29:43 -0500

    comment it out!

--- models/utils/codegen.sql
+++ models/utils/codegen.sql
@@ -1,6 +1,8 @@
-{{ codegen.generate_source(
-    'burst_bank',
+{# used for making sources for `sources.yml` #}
+
+{# {{ codegen.generate_source(
+    'burst_bank_large',
     database_name='glue',
     generate_columns=true,
     include_data_types=true
-    ) }}
+    ) }} #}
